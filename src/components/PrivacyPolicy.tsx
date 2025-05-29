a2cabc46690e1cc8956c824e288c600a7d7c2246
AhmedMaher123384 <ahmedmaher123384@gmail.com>
2025-05-30 01:41:33 +0300

    Initial commit - added project files, ignoring node_modules

--- src/components/PrivacyPolicy.tsx
+++ src/components/PrivacyPolicy.tsx
@@ -292,7 +292,7 @@ const PrivacyPolicy: React.FC = () => {
                     <div className="flex items-center gap-3">
                       <Phone className="w-5 h-5 text-blue-600" />
                       <span className="text-gray-800">رقم الجوال / واتساب: </span>
-                      <span className="font-bold text-blue-600">+966XXXXXXXXX</span>
+                      <span className="font-bold text-blue-600">+966551064118</span>
                     </div>
                   </div>
                 </div>
